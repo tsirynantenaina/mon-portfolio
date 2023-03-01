d3b2586bdc0abc51bf771032af346c31c54b3a1e
tsirynantenaina <tsirynantenaina2708@gmail.com>
2023-03-01 15:51:35 +0100

    correction

--- js/script.js
+++
@@ -1,6 +0,0 @@
-var typed = new Typed(".typing" , {
-    strings:["" , "Developpeur Web" , "Developpeur Mobile" , "Developpeur full-stack" ],
-    typeSpeed:100,
-    BackSpeed:60,
-    loop:true
-})
